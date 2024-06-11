03a0fd75e1b66d1a7751afa1694727a5b4747771
Rafael Santos <rafarag40@gmail.com>
2024-06-11 02:15:54 -0300

    first page create

--- formway/src/App.js
+++ formway/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className="Color-route">
+            <div className="Form-layer">
+                <p>
+                  <h3>
+                      Check List Rotas
+                  </h3>
+                  <button > Iniciar agora </button>
+                </p>
+            </div>
+          </div>
       </header>
     </div>
   );
